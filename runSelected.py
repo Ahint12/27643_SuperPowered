210997eea3d45d6fe5d7ac796406882c53e445c4
ayushi6657 <ayushikontu@gmail.com>
2022-11-09 19:30:55 -0800

    Merge branch 'master' of https://github.com/Ahint12/27643_SuperPowered

--- runSelected.py
+++ runSelected.py
@@ -19,4 +19,49 @@ def runSelected():
         # Start coding your Run here
         print("Starting runSelected()", file=sys.stderr)
 
+        #########################################################
+        # RUN 2: ?? Points
+        #########################################################
+
+        # M08: Watch Television - 20 points
+        driveStraight(35, 510, True)
+        sleep(2)
+        driveStraight(20, 70, True)
+         # M07: Wind Turbine - 30 points
+        driveStraight(-15, 125, True)
+        RWheel.on_for_degrees(20, 185)
+        RWheelShutdown()
+        driveStraight(30, 340, True)
+        '''
+        lineDetect(15, 3, "Black", False)
+        lineDetect(15, 3, "White", True)
+        driveStraight(15, 90, True)
+        LWheel.on_for_degrees(20, 345)
+        LWheelShutdown()
+        driveStraight(20, 250, True)
+        sleep(0.5)
+        driveStraight(-20, 70, True)
+        driveStraight(20, 80, True)
+        sleep(0.5)
+        driveStraight(-20, 70, True)
+        driveStraight(20, 90, True)
+        sleep(0.5)
+        driveStraight(-20, 60, True)
+        driveStraight(20, 90, True)
+        sleep(0.5)
+        driveStraight(-20, 60, True)
+        # sleep(2.0)
+        LWheel.on_for_degrees(-20, 60)
+        LWheelShutdown()
+        # sleep(2.0)
+        driveStraight(-30, 270, True)
+        sleep(0.5)
+        RWheel.on_for_degrees(20, 140)
+        RWheelShutdown()
+        LWheel.on_for_degrees(-20, 150)  
+        LWheelShutdown()
+        driveStraight(-70, 1050, True)
+
+        # Returning to masterProgram(), resetting display.
+        PrintRunNumbersToDisplay()'''
 
