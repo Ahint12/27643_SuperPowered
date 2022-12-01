1af922d9c420275d683b9fd6d40a285e207bbedc
Ahint12 <ashtenhint@gmail.com>
2022-11-30 18:07:06 -0800

    Post mtg Nov-29-2022.

--- runSelected.py
+++ runSelected.py
@@ -14,11 +14,11 @@ from myBlocks import *
 
 
 def Trun6A():
-    motorStall('A', 15, 12)
-    motorStall('D', 7, 5)
+    motorStall('A', -15, -7)
+    motorStall('D', -10, -7)
+    BackMotor.on_for_degrees(10, 60)
+    BackMotorShutdown()
 
-def Trun6B():
-    motorStall('A', -10, -7)
     
 def runSelected():
 
@@ -26,6 +26,40 @@ def runSelected():
         # Start coding your Run here
         print("Starting runSelected()", file=sys.stderr)
 
+        run4A = Thread(target=Trun6A)
+        run4A.start()
+        move_steering.on_for_degrees(0, 30, 180)    
+        turnLineDetect('B', 25, 2, 'Black', True)
+        turnLineDetect('C', 15, 2, 'Black', False)
+        turnLineDetect('C', 15, 2, 'White', True)
+        PLF_Degrees1(2, -1, 500, False)
+        PLF_LineDetect1(2, -1, True)
+        RWheel.on_for_degrees(20, 340)
+        RWheelShutdown()
+        FrontMotor.on_for_degrees(30, 100)
+        FrontMotor.on_for_degrees(-30, 100)
+        FrontMotor.on_for_degrees(30, 100)
+        FrontMotor.on_for_degrees(-30, 100)
+        FrontMotor.on_for_degrees(30, 100)
+        FrontMotor.on_for_degrees(-30, 100)
+        FrontMotorShutdown()
+        #####
+        # M03: Energy Storage - Put 3 Energy Units Into the Energy Storage Bin, Remove Energy Storage Tray - 30 points 10 x 3 energy units
+        # 5 points for removing tray from Energy Storage model.
+        #####
+        lineSquare(-15, "Black", "Left", 0.2)
+        lineSquare(-15, "White", "Left", 0.1)
+        driveStraight(-20, 20, True)
+        RWheel.on_for_degrees(20, 180)
+        RWheelShutdown()
+        driveStraight(25, 45, True)
+        RWheel.on_for_degrees(20, 195)
+        RWheelShutdown()
+        driveStraight(-20, 80, True)
+        BackMotor.on_for_degrees(-20, 37)
+        BackMotorShutdown()
+        move_steering.on_for_degrees(12, 80, 2000)
+        WheelShutdown()
 
 
         '''
